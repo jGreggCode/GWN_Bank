2758e8096fd2056dd90b6676e9dae1b647dd23f6
jGreggCode <felicisimojv@gmail.com>
2023-02-03 08:25:21 +0800

    First Commit

---
+++ ColorPalette.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+
+public class ColorPalette {
+    private final Color colorText = Color.black;
+    private final Color colorBackground = new Color(0x1A1A2E);
+    private final Color colorButtons = new Color(0xE94560);
+
+    public Color getColorText() {
+        return colorText;
+    }
+    public Color getColorBackground() {
+        return colorBackground;
+    }
+    public Color getColorButtons() {
+        return colorButtons;
+    }
+
+}
---
+++ MainFrame.java
@@ -0,0 +1,50 @@
+// Imports
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+// --------
+
+public class MainFrame extends JFrame {
+
+    // Color
+    ColorPalette colors = new ColorPalette();
+    // --------
+
+    // Images
+    ImageIcon logo = new ImageIcon("BankLogoNB128.png");
+
+    // Labels
+    JLabel labelWelcome = new JLabel(), 
+    labelIcon = new JLabel(),
+    labelUsername = new JLabel(),
+    labelPassword = new JLabel();
+    // --------
+
+    // Main frame
+    public MainFrame() {
+        // Main frame configuration
+        this.setTitle("JGBank");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(720, 720);
+        this.setResizable(false);
+        this.getContentPane().setBackground(colors.getColorBackground());
+        // --------
+
+        // Icon Label
+        labelIcon.setIcon(logo);
+        // --------
+
+        // Welcome Label
+        labelWelcome.setText("Welcome To JGBank");
+        labelWelcome.setVerticalAlignment(JLabel.TOP);
+        labelWelcome.setHorizontalAlignment(JLabel.CENTER);
+        // --------
+
+        // Components
+        this.add(labelIcon);
+        this.add(labelWelcome);
+        // --------
+    }
+    // --------
+}
