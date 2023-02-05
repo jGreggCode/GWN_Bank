50321ec8e6b6f0c193ea5eeb275a74cb93ca395f
jGreggCode <118491204+jGreggCode@users.noreply.github.com>
2023-02-05 11:45:10 +0800

    Components

---
+++ Utils/PanelRound.java
@@ -0,0 +1,28 @@
+
+package com.jgfbank.swing;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
+
+
+public class PanelRound extends JPanel {
+    
+    public PanelRound() {
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics grphc) {
+        Graphics2D g2 = (Graphics2D) grphc;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(new Color(255,255,255));
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
+        super.paintComponent(grphc);
+    }
+    
+    
+    
+}
