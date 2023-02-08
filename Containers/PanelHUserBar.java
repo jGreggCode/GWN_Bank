66d3e32f14281ad02737a367cfa75996c08e386e
jGreggCode <felicisimojv@gmail.com>
2023-02-08 17:32:18 +0800

    Changes

--- Containers/PanelHUserBar.java
+++ Containers/PanelHUserBar.java
@@ -1,5 +1,6 @@
 package Containers;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,10 +18,17 @@ public class PanelHUserBar extends JPanel {
     private ColorPalette colorPalette = new ColorPalette();
     private Defaults def = new Defaults();
 
+    // User labels
     private JLabel labelUserIcon = new JLabel(),
     labelUserName = new JLabel(),
     labelAccountStatus = new JLabel(),
     labelUserLogout = new JLabel();
+
+    // Info labels
+    private JLabel labelCash = new JLabel(),
+    labelCashAmmount = new JLabel(),
+    labelCashDollars = new JLabel(),
+    labelCashDollarsAmmount = new JLabel();
 
     private JPanel panelUser = new JPanel() {
         @Override
@@ -60,6 +68,25 @@ public class PanelHUserBar extends JPanel {
         }
     };
 
+    private JPanel panelTrans = new JPanel() {
+        @Override
+        protected void paintComponent(Graphics g) {
+           super.paintComponent(g);
+           Dimension arcs = new Dimension(30,30);
+           int width = getWidth();
+           int height = getHeight();
+           Graphics2D graphics = (Graphics2D) g;
+           graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+   
+   
+           //Draws the rounded opaque panel with borders.
+           graphics.setColor(getBackground());
+           graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);//paint background
+           graphics.setColor(getForeground());
+           graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);//paint border
+        }
+    };
+
     public PanelHUserBar() {
         setOpaque(true);
         setBackground(colorPalette.getColorBackground());
@@ -77,17 +104,33 @@ public class PanelHUserBar extends JPanel {
         labelUserIcon.setIcon(new ImageIcon(getClass().getResource("/Images/man.png")));
         labelUserIcon.setBounds(10, 11, 128, 128);
 
+        String status = "Black Account"; // change later
+
         labelUserName.setText("John Gregg V. Felicisimo");
-        labelAccountStatus.setText("Black Account");
+        labelAccountStatus.setText("Account Status: " + status);
         labelUserLogout.setText("Log out");
 
-        labelUserName.setBounds(150, 42, 452, 20);
+        labelUserName.setBounds(150, 30, 452, 20);
         labelUserName.setForeground(Color.white);
         labelUserName.setFont(new Font(def.getFontFam(), Font.BOLD, 15));
 
+        labelAccountStatus.setBounds(150, 50, 452, 20);
+        labelAccountStatus.setForeground(Color.white);
+        labelAccountStatus.setFont(new Font(def.getFontFam(), Font.BOLD, 15));
+
+        labelUserLogout.setBounds(150, 82, 72, 20);
+        labelUserLogout.setHorizontalAlignment(JLabel.CENTER);
+        labelUserLogout.setForeground(Color.white);
+        labelUserLogout.setOpaque(true);
+        labelUserLogout.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        labelUserLogout.setBackground(colorPalette.getColorButtons());
+        labelUserLogout.setBorder(BorderFactory.createLineBorder(colorPalette.getColorButtons(), 1));
+        labelUserLogout.setFont(new Font(def.getFontFam(), Font.BOLD, 15));
 
         panelUser.add(labelUserName);
         panelUser.add(labelUserIcon);
+        panelUser.add(labelAccountStatus);
+        panelUser.add(labelUserLogout);
         // --------
 
         // Info Panel
@@ -95,11 +138,47 @@ public class PanelHUserBar extends JPanel {
         panelInfo.setOpaque(false);
         panelInfo.setBackground(colorPalette.getColorBackground1());
         panelInfo.setBounds(680, 50, 620, 150);
+
+        labelCash.setText("Balance:");
+        labelCash.setBounds(15, 20, 200, 40);
+        labelCash.setForeground(Color.white);
+        labelCash.setFont(new Font(def.getFontFam(), Font.BOLD, 25));
+
+        double cash = 10000000; // change later
+
+        labelCashAmmount.setText(String.format("PHP %,.2f", cash)); 
+        labelCashAmmount.setHorizontalAlignment(JLabel.TRAILING);
+        labelCashAmmount.setBounds(350, 20, 250, 40);
+        labelCashAmmount.setForeground(Color.white);
+        labelCashAmmount.setFont(new Font(def.getFontFam(), Font.BOLD, 25));
+
+        labelCashDollars.setText("Balance in dollars:");
+        labelCashDollars.setBounds(15, 50, 200, 25);
+        labelCashDollars.setForeground(Color.white);
+        labelCashDollars.setFont(new Font(def.getFontFam(), Font.BOLD, 15));
+
+        labelCashDollarsAmmount.setText(String.format("USD %,.2f", (cash * 54.79))); 
+        labelCashDollarsAmmount.setHorizontalAlignment(JLabel.TRAILING);
+        labelCashDollarsAmmount.setBounds(350, 50, 250, 25);
+        labelCashDollarsAmmount.setForeground(Color.white);
+        labelCashDollarsAmmount.setFont(new Font(def.getFontFam(), Font.BOLD, 15));
+
+        panelInfo.add(labelCash);
+        panelInfo.add(labelCashAmmount);
+        panelInfo.add(labelCashDollars);
+        panelInfo.add(labelCashDollarsAmmount);
         // --------
+
+        // Trans panel
+        panelTrans.setBounds(40, 220, 1260, 700);
+        panelTrans.setLayout(null);
+        panelTrans.setOpaque(false);
+        panelTrans.setBackground(colorPalette.getColorBackground1());
 
 
         add(panelUser);
         add(panelInfo);
+        add(panelTrans);
     }
 }
 
